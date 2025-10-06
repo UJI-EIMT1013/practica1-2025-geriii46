63347b6a0f8b5767cd81188308f3b8a06c70976f
gerard <al428872@uji.es>
2025-10-06 12:20:58 +0200

    ej2

--- Practica1.java
+++ Practica1.java
@@ -26,14 +26,47 @@ public class Practica1 {
     }
 
     //EJERCICIO2
-    public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
-        //TODO
+    public static void separate(Set<Integer> cuadrados, Set<Integer> noCuadrados) {
+        Set<Integer> originalCuadrados = new HashSet<>(cuadrados);
+        Set<Integer> originalNoCuadrados = new HashSet<>(noCuadrados);
+        Set<Integer> universo = new HashSet<>(originalCuadrados);
+        universo.addAll(originalNoCuadrados);
+        Set<Integer> esCuadrado = new HashSet<>();
+        for (Integer base : universo) {
+            if (base != 0) {
+                if (Math.abs(base) <= Integer.MAX_VALUE / Math.abs(base)) {
+                    int cuadrado = base * base;
+                    if (universo.contains(cuadrado)) {
+                        esCuadrado.add(cuadrado);
+                    }
+                }
+            } else {
+                if (universo.contains(0)) {
+                    esCuadrado.add(0);
+                }
+            }
+        }
 
+        if (esCuadrado.contains(1)) {
+            if (!(originalCuadrados.contains(1) && originalNoCuadrados.contains(1))) {
+                esCuadrado.remove(1);
+            }
+        }
+        cuadrados.clear();
+        noCuadrados.clear();
+        for (Integer numero : universo) {
+            if (esCuadrado.contains(numero)) {
+                cuadrados.add(numero);
+            } else {
+                noCuadrados.add(numero);
+            }
+        }
     }
 
     //EJERCICIO 3
     public static<T> Collection<Set<T>> divideInSets (Iterator<T> it) {
         //TODO
+
         return null;
     }
 
